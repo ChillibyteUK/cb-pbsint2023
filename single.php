cbc41f8ca04b6a2e311b7ecbdf350101d5e09f7b
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-13 12:02:35 +0100

    single news

--- single.php
+++ single.php
@@ -21,8 +21,8 @@ $img = get_the_post_thumbnail_url(get_the_ID(),'full');
     <div class="container-xl">
         <div class="row g-4 pb-4">
             <div class="col-lg-9 order-2">
-                <h1 class="blog__title"><?=get_the_title()?></h1>
                 <img src="<?=$img?>" alt="" class="blog__image">
+                <h1 class="blog__title"><?=get_the_title()?></h1>
             <?php
             $count = estimate_reading_time_in_minutes( get_the_content(), 200, true, true );
             echo $count;
@@ -45,8 +45,8 @@ $img = get_the_post_thumbnail_url(get_the_ID(),'full');
                 if ($sidebar) {
                     ?>
                 <div class="sidebar">
-                    <div class="h5 d-none d-lg-block">Quick Links</div>
-                    <div class="h5 d-lg-none" data-bs-toggle="collapse" href="#links" role="button">Quick Links</div>
+                    <div class="h5 d-none d-lg-block text-white">Quick Links</div>
+                    <div class="h5 d-lg-none text-white" data-bs-toggle="collapse" href="#links" role="button">Quick Links</div>
                     <div class="collapse d-lg-block" id="links">
                         <?php
                         foreach ($sidebar as $heading => $id) {
@@ -62,33 +62,46 @@ $img = get_the_post_thumbnail_url(get_the_ID(),'full');
                 ?>
             </div>
         </div>
-        <section class="related pb-5">
-            <h3>Related News</h3>
-            <div class="row g-4">
-            <?php
-            $cats = get_the_category();
-            $ids = wp_list_pluck($cats,'term_id');
-            $r = new WP_Query(array(
-                'category__in' => $ids,
-                'posts_per_page' => 4
-            ));
-            while ($r->have_posts()) {
-                $r->the_post();
+
+    </div>
+    <?php
+    $cats = get_the_category();
+    $ids = wp_list_pluck($cats,'term_id');
+    $r = new WP_Query(array(
+        'category__in' => $ids,
+        'posts_per_page' => 4,
+        'post__not_in' => array(get_the_ID())
+
+    ));
+    if ($r->have_posts()) {
                 ?>
-                <div class="col-md-6 col-xl-3">
-                    <a class="blog_card" href="<?=get_the_permalink()?>">
-                        <img src="<?=get_the_post_thumbnail_url(get_the_ID(),'large')?>" alt="" class="blog_card__image">
-                        <div class="blog_card__content">
-                            <h3 class="blog_card__title"><?=get_the_title()?></h3>
-                        </div>
-                    </a>
+    <section class="latest_news py-5">
+        <div class="container-xl">
+            <div class="latest_news__title">
+                <h2>Related News</h2>
+                <a href="/news/">View all</a>
+            </div>
+            <div class="latest_news__grid">
+
+                <?php
+                while ($r->have_posts()) {
+                    $r->the_post();
+                    ?>
+                <div class="latest_news__card">
+                    <img src="<?=get_the_post_thumbnail_url(get_the_id(), 'large')?>"
+                        alt="">
+                    <h3><?=get_the_title()?></h3>
+                    <a href="<?=get_the_permalink()?>">Learn more</a>
                 </div>
                 <?php
-            }
-            ?>
+                }
+                ?>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+    <?php
+    }
+    ?>
 </main>
 <?php
 get_footer();
