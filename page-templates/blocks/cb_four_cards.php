330316356d1b098c7836f0500f062f4130f1fc33
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-13 13:46:51 +0100

    four cards styles

--- page-templates/blocks/cb_four_cards.php
+++ page-templates/blocks/cb_four_cards.php
@@ -3,25 +3,25 @@
         <div class="row">
             <div class="col-md-6 col-lg-3">
                 <div class="four_cards__card">
-                    <h3 class="<?=get_field('col1_colour')?>"><?=get_field('col1_title')?></h3>
+                    <h3 class="text--<?=get_field('col1_colour')?>"><?=get_field('col1_title')?></h3>
                     <p><?=get_field('col1_content')?></p>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="four_cards__card">
-                    <h3 class="<?=get_field('col2_colour')?>"><?=get_field('col2_title')?></h3>
+                    <h3 class="text--<?=get_field('col2_colour')?>"><?=get_field('col2_title')?></h3>
                     <p><?=get_field('col2_content')?></p>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="four_cards__card">
-                    <h3 class="<?=get_field('col3_colour')?>"><?=get_field('col3_title')?></h3>
+                    <h3 class="text--<?=get_field('col3_colour')?>"><?=get_field('col3_title')?></h3>
                     <p><?=get_field('col3_content')?></p>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="four_cards__card">
-                    <h3 class="<?=get_field('col4_colour')?>"><?=get_field('col4_title')?></h3>
+                    <h3 class="text--<?=get_field('col4_colour')?>"><?=get_field('col4_title')?></h3>
                     <p><?=get_field('col4_content')?></p>
                 </div>
             </div>
