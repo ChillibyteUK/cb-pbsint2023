6713dfc1bdfd3188baa5e45076142e01329fa744
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-13 16:21:19 +0100

    last updated banner

--- page-templates/blocks/cb_banner.php
+++ page-templates/blocks/cb_banner.php
@@ -1,3 +1,3 @@
-<section class="banner has-primary-background-color text-white text-center py-5">
+<section class="banner has-primary-background-color text-white text-center py-4 mb-4">
 Last Updated: <?=get_field('date')?>
 </section>
